48a49af73740b9cdc5f44f22b2bad40144907a39
ItsMeHemant <hemantmhalsekar01@gmail.com>
2023-11-16 20:24:32 +0530

    Completed

--- frontend/src/Components/DescriptionBox/DescriptionBox.jsx
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -3,7 +3,16 @@ import './DescriptionBox.css'
 
 const DescriptionBox = () => {
   return (
-    <div className='descriptionbox'>DescriptionBox</div>
+    <div className='descriptionbox'>
+        <div className='descriptionbox-navigator'>
+            <div className='descriptionbox-nav-box'>Description</div>
+            <div className='descriptionbox-nav-box fade'>Reviews (122)</div>
+        </div>
+        <div className='descriptionbox-description'>
+            <p>In the age of digitalization, an ecommerce website has become an essential tool for businesses of all sizes. It serves as a virtual storefront, showcasing products or services to a global audience and facilitating seamless transactions online. An ecommerce website typically consists of a product catalog with detailed descriptions, high-quality images, and pricing information. Customers can browse through the catalog, add items to their shopping cart, and proceed to checkout, where they can choose from secure payment methods and arrange for shipping or delivery. Ecommerce websites also offer features like customer accounts, order tracking, and feedback mechanisms to enhance the shopping experience.</p>
+            <p>An eCommerce website, or electronic commerce website, is an online platform that allows businesses to sell products or services to customers over the internet. It is essentially a digital storefront that mimics the experience of shopping at a physical store.</p>
+        </div>
+    </div>
   )
 }
 
