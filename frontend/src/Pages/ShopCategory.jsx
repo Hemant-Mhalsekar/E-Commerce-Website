7af4bf2c67001009547f3830b2819e00b95e8f0d
ItsMeHemant <hemantmhalsekar01@gmail.com>
2023-11-16 11:51:44 +0530

    Completed shop category

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -8,7 +8,7 @@ const ShopCategory = (props) => {
   const {all_product} = useContext(ShopContext);
   return (
     <div className='shop-category'>
-      <img src={props.banner} alt=''/>
+      <img className='shopcategory-banner' src={props.banner} alt=''/>
       <div className='shopcategory-indexSort'>
         <p>
           <span>Showing 1-12</span> out of 36 products
@@ -20,9 +20,14 @@ const ShopCategory = (props) => {
       <div className='shopcategory-products'>
         {all_product.map((item,i)=>{
           if (props.category===item.category) {
-            return <Item/>
+            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+          } else {
+            return null;
           }
         })}
+      </div>
+      <div className='shopcategory-loadmore'>
+        Explore More
       </div>
     </div>
   )
