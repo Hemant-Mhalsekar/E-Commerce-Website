79ded102fbbda26beaeb99a697e4c1ab34f15663
ItsMeHemant <hemantmhalsekar01@gmail.com>
2023-11-16 12:23:50 +0530

    Working on login signup

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import "./CSS/LoginSignup.css"
 
 function LoginSignup() {
   return (
-    <div>loginSignup</div>
+    <div className='loginsignup'>
+      <div className='loginsignup-container'>
+        <h1>Sign up</h1>
+        <div className='loginsignup-fields'>
+          <input type='text' placeholder='Your Name'/>
+          <input type='email' placeholder='Email Adderss'/>
+          <input type='password' placeholder='Password'/>
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Already have an account? <span> Login here</span></p>
+        <div className='loginsignup-agree'>
+          <input type='checkbox' name='' id=''/>
+          <p>By continuing, I agree to terms and conditions & privacy policy.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
