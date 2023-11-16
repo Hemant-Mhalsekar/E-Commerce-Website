07cb81397f95d2597c1acb622d481b767a73fa72
ItsMeHemant <hemantmhalsekar01@gmail.com>
2023-11-16 16:08:45 +0530

    working on prosuct display

---
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ProductDisplay.css'
+
+const ProductDisplay = () => {
+  return (
+    <div className='productdisplay'>ProductDisplay</div>
+  )
+}
+
+export default ProductDisplay
