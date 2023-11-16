b738e58ca96d4ecdc6ae06283549476aabee2bd7
ItsMeHemant <hemantmhalsekar01@gmail.com>
2023-11-16 15:55:48 +0530

    completed breadcrums

--- frontend/src/Components/Breadcrums/Breadcrum.jsx
+++ frontend/src/Components/Breadcrums/Breadcrum.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Breadcrum.css'
+import arrow_icon from '../Assets/breadcrum_arrow.png'
 
-const Breadcrum = () => {
+const Breadcrum = (props) => {
+    const {product} = props;
   return (
-    <div className='breadcrum'>Breadcrum</div>
+    <div className='breadcrum'>
+    HOME <img src={arrow_icon} alt=''/> Shop <img src={arrow_icon} alt=''/> {product.catrgory} <img src={arrow_icon} alt=''/> {product.name}
+    </div>
   )
 }
 
